0bf896b554b807f205307c001a623820171bcb64
pablo ramos <pabloramosdev@gmail.com>
2024-04-21 13:36:53 +0200

    feat: PriceRepository use in Controller

--- src/main/java/com/pabloramosdev/product/controller/PriceController.java
+++ src/main/java/com/pabloramosdev/product/controller/PriceController.java
@@ -1,29 +1,37 @@
 package com.pabloramosdev.product.controller;
 
 import com.pabloramosdev.product.model.PriceDto;
+import com.pabloramosdev.product.persistence.entity.Price;
+import com.pabloramosdev.product.persistence.repository.PriceRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.math.BigDecimal;
 import java.time.LocalDateTime;
-import java.time.Period;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping(path = "/prices")
 public class PriceController {
 
+    private final PriceRepository priceRepository;
+
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<PriceDto> getPrices() {
+    public ResponseEntity<PriceDto> getPrices(@RequestParam("fare_date") LocalDateTime fareDate,
+                                              @RequestParam("product_id") Integer productId,
+                                              @RequestParam("brand_id") Integer brandId) {
+        Price price = priceRepository.findProductPriceByBrandAndDateRange(fareDate, productId, brandId);
         return ResponseEntity.ok().body(PriceDto.builder()
-                        .brandId(1)
-                        .productId(35455)
-                        .fare(1)
-                        .startDate(LocalDateTime.now())
-                        .endDate(LocalDateTime.now().plus(Period.ofMonths(1)))
-                        .price(new BigDecimal("35.50"))
+                        .brandId(price.getBrandId())
+                        .productId(price.getProductId())
+                        .fare(price.getPriceList())
+                        .startDate(price.getStartDate())
+                        .endDate(price.getEndDate())
+                        .price(price.getPrice())
                 .build());
     }
 
